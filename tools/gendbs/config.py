aed3e633a7f176b4d8c665092cf197931604b3e0
andy.mei <andy.mei@momenta.ai>
2022-12-09 21:16:23 +0800

    add gendbs and mysql orm

--- tools/gendbs/config.py
+++ tools/gendbs/config.py
@@ -1,15 +1,15 @@
 # MYSQL
-IP = "127.0.0.1"
+IP = '127.0.0.1'
 PORT = 3306
-USERNAME = "momenta"
-PASSWORD = "momenta"
-DATABASE = "ct_db"
+USERNAME = 'momenta'
+PASSWORD = 'momenta'
+DATABASE = 'ct_db'
 
-ORM = "sqlalchemy"
+ORM = 'sqlalchemy'
 
 # 目标文件
-MODEL_OUTDIR      = "../../app/model"             # 模型文件输出目录
-MODEL_INIT_OUTDIR = MODEL_OUTDIR +"/__init__.py"  # 模型文件输出目录
+MODEL_OUTDIR      = '../../app/model'             # 模型文件输出目录
+MODEL_INIT_OUTDIR = MODEL_OUTDIR +'/__init__.py'  # 模型文件输出目录
 
 # 模板文件路径
 MODEL_TEMP        = f'./template/{ORM}/model.template'
@@ -17,4 +17,4 @@ MODEL_INIT_TEMP   = f'./template/__init__.template'
 MODEL_README_TEMP = './template/README.md.template'
 
 # 导包路径
-IMPORT_INIT_PATH = f'from {".".join(MODEL_OUTDIR.split("/")[2:5])}.'   # app.model.dbs
+IMPORT_INIT_PATH = f'from {'.'.join(MODEL_OUTDIR.split('/')[2:5])}.'   # app.model.dbs
